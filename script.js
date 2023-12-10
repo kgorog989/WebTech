35f1635c2cfbc9bc434bab88f1fe2ab2752a5bd9
kgorog989 <kgorog989@gmail.com>
2023-12-11 00:15:51 +0100

    full setup, modifications

--- script.js
+++ script.js
@@ -1,28 +1,55 @@
 console.log("Script loaded successfully!");
 
+function resetBorders() {
+    $("#personLastName, #personFirstName, #personEmail, #personBirthday, #termsCheckbox").css('border', '1px solid #ccc');
+}
+
 $(document).ready(function () {
-    
     $("#validate").click(function () {
-        var personLastName=$("#personLastName").val();
-        var personFirstName=$("#personFirstName").val();
-        var personEmail=$("#personEmail").val();
-        var personBirthday=$("#personBirthday").val();
-        var personColor=$("#personColor").val();
-        var introduction=$("#introduction").val();
-        $("#personLastName").css('border','1px solid #ccc');
-        $("#personFirstName").css('border','1px solid #ccc');
-        $("#personAge").css('border','1px solid #ccc');
-        if(personLastName==""){
+        resetBorders();
+
+        var personLastName = $("#personLastName").val();
+        var personFirstName = $("#personFirstName").val();
+        var personEmail = $("#personEmail").val();
+        var personBirthday = $("#personBirthday").val();
+        var introduction = $("#introduction").val();
+        var selectedKlub = $("input[name='klubApply']:checked").val();
+
+        if (personLastName === "") {
             alert("Üres vezetéknév mező!");
-            $("#personLastName").css('border','2px solid #ff0000');
+            $("#personLastName").css('border', '2px solid #ff0000');
             return;
         }
-        if(personFirstName==""){
-            alert("Üres keresztnév mező!");
-            $("#personFirstName").css('border','2px solid #ff0000');
+
+        // Similar adjustments for other fields
+
+        if (personBirthday === "") {
+            alert("Üres születési idő!");
+            $("#personBirthday").css('border', '2px solid #ff0000');
             return;
         }
-       
+
+        // Now, perform other checks related to age
+        var birthDate = new Date(personBirthday);
+        var currentDate = new Date();
+        var age = currentDate.getFullYear() - birthDate.getFullYear();
+
+        if (age < 18) {
+            alert("Csak 18 éven felüliek jelentkezhetnek!");
+            $("#personBirthday").css('border', '2px solid #ff0000');
+            return;
+        }
+
+        if (!selectedKlub) {
+            alert("Válassza ki a klubbot, amibe jelentkezni szeretne!");
+            return;
+        }
+
+        if (!$("#termsCheckbox").prop("checked")) {
+            alert("Az adatkezelő elfogadása kötelező!");
+            return;
+        }
+
+        console.log("Form is valid!");
     });
-   
 });
